1ee181381d21891c917795b0b8b1ac5745671773
Zeta Lockhart <aixoahiryu@gmail.com>
2019-03-17 16:49:33 +0700

    Implemented 'Add Entry'

--- Symfonews/src/Controller/AdminController.php
+++ Symfonews/src/Controller/AdminController.php
@@ -6,23 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+
 use App\Entity\Author;
 use App\Form\AuthorFormType;
+use App\Entity\BlogPost;
+use App\Form\EntryFormType;
 
+/**
+ * @Route("/admin")
+ */
 class AdminController extends AbstractController
 {
-    /**
-     * @Route("/admin", name="admin")
-     */
-    public function index()
-    {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
-	}
 
 	/**
-	 * @Route("/admin/author/create", name="author_create")
+	 * @Route("/author/create", name="author_create")
 	 */
 	public function createAuthorAction(Request $request)
 	{
@@ -51,6 +48,59 @@ class AdminController extends AbstractController
 
 	    return $this->render('admin/create_author.html.twig', [
 	        'form' => $form->createView()
+	    ]);
+	}
+
+	/**
+	 * @Route("/create-entry", name="admin_create_entry")
+	 *
+	 * @param Request $request
+	 *
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function createEntryAction(Request $request)
+	{
+	    $blogPost = new BlogPost();
+
+	    $author = $this->authorRepository->findOneByUsername($this->getUser()->getUserName());
+	    $blogPost->setAuthor($author);
+
+	    $form = $this->createForm(EntryFormType::class, $blogPost);
+	    $form->handleRequest($request);
+
+	    // Check is valid
+	    if ($form->isSubmitted() && $form->isValid()) {
+	        $this->entityManager->persist($blogPost);
+	        $this->entityManager->flush($blogPost);
+
+	        $this->addFlash('success', 'Congratulations! Your post is created');
+
+	        return $this->redirectToRoute('admin_entries');
+	    }
+
+	    return $this->render('admin/entry_form.html.twig', [
+	        'form' => $form->createView()
+	    ]);
+	}
+
+	/**
+	 * @Route("/", name="admin_index")
+	 * @Route("/entries", name="admin_entries")
+	 *
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function entriesAction()
+	{
+	    $author = $this->authorRepository->findOneByUsername($this->getUser()->getUserName());
+
+	    $blogPosts = [];
+
+	    if ($author) {
+	        $blogPosts = $this->blogPostRepository->findByAuthor($author);
+	    }
+
+	    return $this->render('admin/entries.html.twig', [
+	        'blogPosts' => $blogPosts
 	    ]);
 	}
 	
--- Symfonews/src/Controller/BlogController.php
+++ Symfonews/src/Controller/BlogController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+	/** @var EntityManagerInterface */
+	private $entityManager;
+
+	/** @var \Doctrine\Common\Persistence\ObjectRepository */
+	private $authorRepository;
+
+	/** @var \Doctrine\Common\Persistence\ObjectRepository */
+	private $blogPostRepository;
+
+	/**
+	 * @param EntityManagerInterface $entityManager
+	 */
+	public function __construct(EntityManagerInterface $entityManager)
+	{
+	    $this->entityManager = $entityManager;
+	    $this->blogPostRepository = $entityManager->getRepository('App:BlogPost');
+	    $this->authorRepository = $entityManager->getRepository('App:Author');
+	}
+
     /**
-     * @Route("/", name="homepage")
-     */
-    public function index()
-    {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
-    }
+	 * @Route("/", name="homepage")
+	 * @Route("/entries", name="entries")
+	 */
+	public function entriesAction()
+	{
+	    return $this->render('blog/entries.html.twig', [
+	        'blogPosts' => $this->blogPostRepository->findAll()
+	    ]);
+	}
 }
