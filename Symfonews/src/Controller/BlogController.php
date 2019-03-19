c4ea5fd2a1ed4d1e48f04f1a3c5f7469c4f343f0
Zeta Lockhart <aixoahiryu@gmail.com>
2019-03-19 16:56:29 +0700

    Fixing HWIOauth0

--- Symfonews/src/Controller/BlogController.php
+++ Symfonews/src/Controller/BlogController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends AbstractController
